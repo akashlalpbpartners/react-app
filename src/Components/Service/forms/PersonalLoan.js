79c7e812ffbf049958edf8a7649eb3cf4d6420ff
deepaman07 <123161686+deepaman07@users.noreply.github.com>
2023-03-01 16:59:43 +0530

    changes

--- src/Components/Service/forms/PersonalLoan.js
+++ src/Components/Service/forms/PersonalLoan.js
@@ -21,10 +21,9 @@ const PersonalLoan = (props) => {
   const { user, city, empType } = context;
   const [loanLeadDetails, setLoanLeadDetails] = useState([]);
   const [flag, setFlag] = useState(0);
-  const [error, setError] = useState({
-    firstNo: "",
-    loanAmount: "",
-  });
+  const [mobileError, setMobileError] = useState("empty");
+  const [loanError, setLoanError] = useState("empty");
+  const [showAlert, setShowAlert] = useState(false);
   const [otp, setOtp] = useState("");
   const [disableOn, setDisableOn] = useState(true);
   const [toggleShow, setToggleShow] = useState(false);
@@ -73,31 +72,31 @@ const PersonalLoan = (props) => {
     },
     validationSchema: validationSchemaInput,
     onSubmit: async (values) => {
-      console.log("hello");
-      // const isPresent = [];
-      // loanLeadDetails.filter((row) => {
-      //   if (row.CustomerMobile === parseInt(values.CustomerMobile))
-      //     isPresent.push(row);
-      //   return 0;
-      // });
-      // const requestOptions = {
-      //   method: "POST",
-      //   headers: {
-      //     Authorization: `Bearer ${Token}`,
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify({
-      //     SubProductId: parseInt(props.ToggleSubForm),
-      //     CustomerMobile: parseInt(values.CustomerMobile),
-      //     CityId: values.City,
-      //     LoanAmount: parseInt(values.LoanAmount),
-      //     NetMonthlyIncome: parseInt(values.NetMonthlyIncome),
-      //     EmploymentType: values.EmploymentType,
-      //     FINCode: JSON.parse(Cookies.get("userCookie")).FINCode,
-      //     GrossSales: 0,
-      //     IsPresent: isPresent.length === 0 ? 0 : 1,
-      //   }),
-      // };
+      const isPresent = [];
+      loanLeadDetails.filter((row) => {
+        if (row.CustomerMobile === parseInt(values.CustomerMobile))
+          isPresent.push(row);
+        return 0;
+      });
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${user.Token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          SubProductId: parseInt(props.ToggleSubForm),
+          CustomerMobile: parseInt(values.CustomerMobile),
+          CityId: values.City,
+          LoanAmount: parseInt(values.LoanAmount),
+          NetMonthlyIncome: parseInt(values.NetMonthlyIncome),
+          EmploymentType: values.EmploymentType,
+          FINCode: JSON.parse(Cookies.get("userCookie")).FINCode,
+          GrossSales: 0,
+          IsPresent: isPresent.length === 0 ? 0 : 1,
+        }),
+      };
+      console.log(requestOptions.body);
       // await fetch(
       //   "http://localhost:3001/product/insertfinancialservices",
       //   requestOptions
@@ -105,65 +104,6 @@ const PersonalLoan = (props) => {
       // formikInput.handleReset();
     },
   });
-  const handleMobileNumber = (e) => {
-    let firstNumber = e.target.value[0];
-    if (firstNumber < 5) setError("first");
-    else setError("");
-    if (e.target.value.length > e.target.maxLength) {
-      e.target.value = e.target.value.slice(0, e.target.maxLength);
-    }
-  };
-
-  const handleLoanAmount = (e) => {
-    if (e.target.value.length > e.target.maxLength) setError("Invalid Amount");
-    else setError("");
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const isPresent = [];
-    loanLeadDetails.filter((row) => {
-      if (row.CustomerMobile === parseInt(e.target[0].value))
-        isPresent.push(row);
-      return 0;
-    });
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${Token}`,
-        "Content-Type": "application/json",
-      },
-
-      body: JSON.stringify({
-        SubProductId: parseInt(props.ToggleSubForm),
-        CustomerMobile: parseInt(e.target[0].value),
-        CityId: parseInt(e.target[1].value),
-        LoanAmount: parseInt(e.target[2].value),
-        NetMonthlyIncome: parseInt(e.target[3].value),
-        EmploymentType: parseInt(e.target[4].value),
-        FINCode: JSON.parse(Cookies.get("userCookie")).FINCode,
-        GrossSales: 0,
-        IsPresent: isPresent.length === 0 ? 0 : 1,
-      }),
-    };
-    // await fetch(
-    //   "http://localhost:3001/product/insertfinancialservices",
-    //   requestOptions
-    // );
-    console.log(flag);
-    e.target.reset();
-    console.log(requestOptions.body);
-  };
-
-  const otpField = {
-    1: ["field-1"],
-    2: ["field-2"],
-    3: ["field-3"],
-    4: ["field-4"],
-    5: ["field-5"],
-    6: ["field-6"],
-  };
-
   const setInputField = (otp) => {
     for (let i = 0; i < 6; i++) {
       if (i < otp.length)
@@ -180,41 +120,18 @@ const PersonalLoan = (props) => {
     nextfield.focus();
   };
 
-  const handleChangeOtp = (e) => {
-    e.target.value = e.target.value.replace(/[^0-9]/g, "");
-    if (e.key === "Backspace") {
-      setOtp(otp.substring(0, otp.length - 1));
-    }
-    if (
-      otp.length <= 5 &&
-      (e.key === "1" ||
-        e.key === "2" ||
-        e.key === "3" ||
-        e.key === "4" ||
-        e.key === "5" ||
-        e.key === "6" ||
-        e.key === "7" ||
-        e.key === "8" ||
-        e.key === "9" ||
-        e.key === "0")
-    ) {
-      let str = otp + e.key;
-      setOtp(str);
-      if (otp.length === 5) setDisableOn(false);
-    }
-  };
   ////////////////////////// Attribute dictionary //////////////////////////
   const inputField = {
     1: [
-      "Mobile_no",
+      "CustomerMobile",
       "Mobile Number",
       "Enter Mobile Number",
-      formikInput.values.Mobile_no,
-      formikInput.touched.Mobile_no && Boolean(formikInput.errors.Mobile_no),
-      formikInput.touched.Mobile_no && formikInput.errors.Mobile_no,
+      formikInput.values.CustomerMobile,
+      formikInput.touched.CustomerMobile &&
+        Boolean(formikInput.errors.CustomerMobile),
+      formikInput.touched.CustomerMobile && formikInput.errors.CustomerMobile,
       false,
       [],
-      formikInput.handleMobile,
     ],
     2: [
       "City",
@@ -227,38 +144,35 @@ const PersonalLoan = (props) => {
       cityList,
     ],
     3: [
-      "Loan_amount_required",
+      "LoanAmount",
       "Loan Amount Required",
       "Enter Loan amount required",
-      formikInput.values.Loan_amount_required,
-      formikInput.touched.Loan_amount_required &&
-        Boolean(formikInput.errors.Loan_amount_required),
-      formikInput.touched.Loan_amount_required &&
-        formikInput.errors.Loan_amount_required,
+      formikInput.values.LoanAmount,
+      formikInput.touched.LoanAmount && Boolean(formikInput.errors.LoanAmount),
+      formikInput.touched.LoanAmount && formikInput.errors.LoanAmount,
       false,
       [],
-      // handleLoanAmount,
     ],
     4: [
-      "Net_monthly_income",
+      "NetMonthlyIncome",
       "Net Monthly Income",
       "Enter Net monthly income",
-      formikInput.values.Net_monthly_income,
-      formikInput.touched.Net_monthly_income &&
-        Boolean(formikInput.errors.Net_monthly_income),
-      formikInput.touched.Net_monthly_income &&
-        formikInput.errors.Net_monthly_income,
+      formikInput.values.NetMonthlyIncome,
+      formikInput.touched.NetMonthlyIncome &&
+        Boolean(formikInput.errors.NetMonthlyIncome),
+      formikInput.touched.NetMonthlyIncome &&
+        formikInput.errors.NetMonthlyIncome,
       false,
       [],
     ],
     5: [
-      "Employment_type",
+      "EmploymentType",
       "Employment Type",
       "Enter Employment type",
-      formikInput.values.Employment_type,
-      formikInput.touched.Employment_type &&
-        Boolean(formikInput.errors.Employment_type),
-      formikInput.touched.Employment_type && formikInput.errors.Employment_type,
+      formikInput.values.EmploymentType,
+      formikInput.touched.EmploymentType &&
+        Boolean(formikInput.errors.EmploymentType),
+      formikInput.touched.EmploymentType && formikInput.errors.EmploymentType,
       true,
       empTypeList,
     ],
@@ -277,117 +191,44 @@ const PersonalLoan = (props) => {
         >
           <form
             className="container"
-            onSubmit={handleSubmit}
+            id="my-form"
+            onSubmit={formikInput.handleSubmit}
             autoComplete="off"
           >
             <div
-              className="tab-pane fade show active "
+              className="tab-pane fade show active"
               id="personal-loan"
               role="tabpanel"
               aria-labelledby="pills-home-tab"
             >
               <div className="row">
-                <div className="col-md-4">
-                  <div className="form-floating mb-3">
-                    <input
-                      type="number"
-                      className="form-control"
-                      id="CustomerMoblie"
-                      name="CustomerMoblie"
-                      placeholder="Mobile Number"
-                      maxLength={10}
-                      onChange={handleMobileNumber}
-                      onKeyDown={(evt) =>
-                        ["e", "E", "+", "-"].includes(evt.key) &&
-                        evt.preventDefault()
-                      }
-                    />
-                    <label htmlFor="floatingInput">Mobile Number</label>
-                    {error === "first" ? (
-                      <small className="invalid error_messages">
-                        Invalid Mobile Number
-                      </small>
-                    ) : (
-                      <></>
-                    )}
-                  </div>
-                </div>
-                <div className="col-md-4">
-                  <div class="form-floating mb-3">
-                    <select class="form-select" id="City" name="City" required>
-                      <option selected>City</option>
-                      {cityList.map((value, key) => (
-                        <option name="City" key={key} value={value[0]}>
-                          {value[1]}
-                        </option>
-                      ))}
-                    </select>
-                    <label htmlFor="floatingInput">City</label>
-                  </div>
-                </div>
-                <div className="col-md-4">
-                  <div className="form-floating mb-3">
-                    <input
-                      type="number"
-                      className="form-control"
-                      id="LoanAmount"
-                      name="LoanAmount"
-                      placeholder="Loan Amount"
-                      maxLength={7}
-                      onChange={handleLoanAmount}
-                      onKeyDown={(evt) =>
-                        ["e", "E", "+", "-"].includes(evt.key) &&
-                        evt.preventDefault()
-                      }
-                    />
-                    <label htmlFor="loan_amount">Loan Amount</label>
-                    {error === "Invalid Amount" ? (
-                      <small className="invalid error_messages">
-                        Invalid Loan Amount
-                      </small>
-                    ) : (
-                      <></>
-                    )}
-                  </div>
-                </div>
-                <div className="col-md-4">
-                  <div class="form-floating mb-3">
-                    <input
-                      type="number"
-                      className="form-control"
-                      id="NetMonthlyIncome"
-                      name="NetMonthlyIncome"
-                      onChange={handleMobileNumber}
-                      onKeyDown={(evt) =>
-                        ["e", "E", "+", "-"].includes(evt.key) &&
-                        evt.preventDefault()
-                      }
-                    />
-                    <label htmlFor="floatingInput">Net Monthly Income</label>
-                  </div>
-                </div>
-                <div className="col-md-4">
-                  <div class="form-floating mb-3">
-                    <select
+                {Object.entries(inputField).map(([key, item]) => (
+                  <>
+                    <TextField
+                      key={key}
+                      fullWidth
                       required
-                      class="form-select"
-                      id="EmploymentType"
-                      name="EmploymentType"
-                      aria-label="Default select example"
+                      id={item[0]}
+                      select={item[6]}
+                      name={item[0]}
+                      label={item[1]}
+                      placeholder={item[2]}
+                      value={item[3]}
+                      onChange={formikInput.handleChange}
+                      error={item[4]}
+                      helperText={item[5]}
                     >
-                      <option selected>Employment Type</option>
-                      {empTypeList.map((value, key) => (
-                        <option
-                          name="EmploymentType"
-                          key={key}
-                          value={value[0]}
-                        >
+                      <MenuItem value="">
+                        <em>{item[1]}</em>
+                      </MenuItem>
+                      {item[7].map((value, key) => (
+                        <MenuItem name={item[1]} key={key} value={value[0]}>
                           {value[1]}
-                        </option>
+                        </MenuItem>
                       ))}
-                    </select>
-                  </div>
-                </div>
+                    </TextField>
+                  </>
+                ))}
               </div>
             </div>
             <div className="row">
@@ -395,22 +236,19 @@ const PersonalLoan = (props) => {
                 <div className="service-footer">
                   <div className="service-footer-btn">
                     <button
-                      type="reset"
+                      type="button"
                       className="btn btn-outline-secondary"
+                      onClick={formikInput.handleReset}
                       form="my-form"
-                      onClick={() => {
-                        setError("");
-                      }}
                     >
                       Reset
                     </button>
                     <button
-                      type="submit"
-                      data-bs-toggle="modal"
-                      data-bs-target="#exampleModal"
+                      type="button"
                       className="btn btn-primary"
+                      onClick={formikInput.handleSubmit}
                     >
-                      Submit
+                      Submit Request
                     </button>
                   </div>
                 </div>
@@ -418,62 +256,6 @@ const PersonalLoan = (props) => {
             </div>
           </form>
         </Box>
-      </div>
-      <div
-        class="modal fade"
-        id="exampleModal"
-        tabindex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">
-                Verify Lead
-              </h1>
-              <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div class="modal-body">
-              <h6>Please enter the otp</h6>
-              <div className="otp-number">
-                <ul>
-                  {Object.entries(otpField).map(([key, value]) => (
-                    <li key={key}>
-                      <input
-                        id={value[0]}
-                        type="number"
-                        className="form-control"
-                        maxLength={1}
-                        onInput={(e) => {
-                          Check.maxLengthCheck(e);
-                        }}
-                        onKeyUp={handleChangeOtp}
-                      />
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-            <div class="modal-footer">
-              <button
-                type="button"
-                class="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                Close
-              </button>
-              <button type="button" class="btn btn-primary">
-                Save changes
-              </button>
-            </div>
-          </div>
-        </div>
       </div>
     </>
   );
